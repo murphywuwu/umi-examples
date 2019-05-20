41807870ef5ed6a86df230bcd93ba5d326409732
murphywuwu <murphywuwu@gmail.com>
2019-05-20 19:02:04 +0800

    💡 (runtime-routes-appjs) 运行时配置: patchRoutes & render

--- runtime-routes-appjs/src/app.js
+++ runtime-routes-appjs/src/app.js
@@ -1,5 +1,6 @@
 import fetch from 'dva/fetch';
-
+import { Server } from 'net';
+/* umi规定src目录下的app.js为运行时配置文件 */
 export const dva = {
   config: {
     onError(err) {
@@ -12,10 +13,16 @@ export const dva = {
 let authRoutes = null;
 let serveRoutes = null;
 
+/*
+ * patchRoutes:用于运行时修改路由
+ * tips: 支持修改routes就好，不要返回新的路由对象
+ * 参数:
+ * routes: Array, 路由配置
+*/
 export function patchRoutes(routes) {
-
+  console.log('routes', routes);
   routes.forEach(element => {
-    //add route
+
     serveRoutes.map(sr => {
       if (element.path === sr.path) {
         sr.addRoutes.map(srItem => {
@@ -23,11 +30,14 @@ export function patchRoutes(routes) {
             path: srItem.path,
             component: require('./pages' + srItem.path).default,
           };
+          // 请求服务端根据响应，动态添加路由
           element.routes && element.routes.unshift(newRoute);
         });
       }
     });
-    //add auth
+
+    // authRoutes: { "/": {role: 'admin'} }
+    // 请求服务端根据响应，动态给特定路由注入权限数据
     Object.assign(element,authRoutes[element.path] || {})
     element.routes &&
       element.routes.forEach(route => {
@@ -37,15 +47,23 @@ export function patchRoutes(routes) {
   window.g_routes = routes;
 }
 
+/*
+ * 用于改写整个应用render到dom树的方法
+ * 参数:
+ * oldRender: Function, 原始render方法，需至少被调用一次
+ */
 export function render(oldRender) {
+  // 渲染应用之前做权限校验，不通过则跳转到登录页
   fetch('/api/auth_routes')
     .then(res => res.json())
     .then(ret => {
       authRoutes = ret;
+      console.log('authRoutes', authRoutes)
       fetch('/api/routes')
         .then(res => res.json())
         .then(data => {
           serveRoutes = data;
+          console.log('serveRoutes', serveRoutes);
           oldRender();
         });
     });
